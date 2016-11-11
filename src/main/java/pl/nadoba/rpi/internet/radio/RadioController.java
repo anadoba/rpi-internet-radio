3b6507087b89c391f44aca0287fc764660656dc1
Adam Nadoba <anadoba@windowslive.com>
2016-11-11 13:01:15 +0100

    channel up&down, volume up&down, new radio stations supported

--- src/main/java/pl/nadoba/rpi/internet/radio/RadioController.java
+++ src/main/java/pl/nadoba/rpi/internet/radio/RadioController.java
@@ -6,19 +6,28 @@ import org.lirc.util.SimpleLIRCClient;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class RadioController {
 
+    private final static String pulseAudioDeviceId = "1";
+    private final static int startupVolume = 70;
+    private final static int volumeStep = 10;
+    private final static int maxVolume = 150;
+    private final static int minVolume = 1;
     private final static Logger logger = Logger.getLogger(RadioController.class);
-
     private final File configFile = new File(this.getClass().getClassLoader().getResource("internetRadio.lirc").getFile());
 
+    private int volume;
     private SimpleLIRCClient lirc;
     private InternetRadio radio;
 
     public RadioController(InternetRadio radio) {
         try {
             lirc = new SimpleLIRCClient(configFile);
+            String[] setVolumeArgs = new String[] {"pactl", "set-sink-volume", pulseAudioDeviceId, startupVolume + "%"};
+            new ProcessBuilder(setVolumeArgs).start();
+            volume = startupVolume;
             lirc.addIRActionListener(commandString -> {
                 final RemoteControlCommand command = RemoteControlCommand.valueOf(commandString);
                 logger.debug("Remote button " + command + " pressed.");
@@ -36,10 +45,28 @@ public class RadioController {
                     case NEXT_CH:
                         radio.playNextStation();
                         break;
-                    // TODO: execute volume-related commands below, possibly add some private field with current volume
                     case VOL_DOWN:
+                        int decreasedVolume = volume - volumeStep;
+                        if (decreasedVolume < minVolume) break;
+
+                        String[] decreaseVolumeArgs = new String[] {"pactl", "set-sink-volume", pulseAudioDeviceId, "--", "-" +decreasedVolume + "%"};
+                        try {
+                            new ProcessBuilder(decreaseVolumeArgs).start();
+                            volume = decreasedVolume;
+                        } catch (IOException e) {
+                            logger.error("Error when decreasing volume - " + Arrays.toString(decreaseVolumeArgs), e);
+                        }
                         break;
                     case VOL_UP:
+                        int increasedVolume = volume + volumeStep;
+                        if (increasedVolume > maxVolume) break;
+                        String[] increaseVolumeArgs = new String[] {"pactl", "set-sink-volume", pulseAudioDeviceId, "--", "+" +increasedVolume + "%"};
+                        try {
+                            new ProcessBuilder(increaseVolumeArgs).start();
+                            volume = increasedVolume;
+                        } catch (IOException e) {
+                            logger.error("Error when increasing volume - " + Arrays.toString(increaseVolumeArgs), e);
+                        }
                         break;
                     default: // used by CH buttons
                         radio.playStation(command);
@@ -49,7 +76,7 @@ public class RadioController {
         } catch (LIRCException e) {
             logger.error("LIRC failure - cfg file is " + configFile.toString(), e);
         } catch (IOException e) {
-            logger.error("IO exception when reading the config file " + configFile.toString(), e);
+            logger.error("IO exception when reading the config file " + configFile.toString() + " or setting startup volume - " + startupVolume, e);
         }
     }
 }
--- src/main/java/pl/nadoba/rpi/internet/radio/RadioStations.java
+++ src/main/java/pl/nadoba/rpi/internet/radio/RadioStations.java
@@ -2,8 +2,10 @@ package pl.nadoba.rpi.internet.radio;
 
 import org.apache.log4j.Logger;
 
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,8 +21,15 @@ public class RadioStations {
     public RadioStations() {
         try {
             stations = new HashMap<RemoteControlCommand, URL>() {{
-                put(RemoteControlCommand.CH1, new URL("http://n-2-5.dcs.redcdn.pl/sc/o2/Eurozet/live/antyradio.livx?audio=5"));
-                put(RemoteControlCommand.CH2, new URL("http://radio.flex.ru:8000/radionami"));
+                put(RemoteControlCommand.CH1, /* ANTY RADIO */ new URL("http://n-2-5.dcs.redcdn.pl/sc/o2/Eurozet/live/antyradio.livx?audio=5"));
+                put(RemoteControlCommand.CH2, /* ESKA ROCK */ new URL("http://waw04.ic2.scdn.smcloud.net/t041-1.mp3"));
+                put(RemoteControlCommand.CH3, /* RADIO ZET */ new URL("http://n-2-15.dcs.redcdn.pl/sc/o2/Eurozet/live/audio.livx?audio=5"));
+                put(RemoteControlCommand.CH4, /* RMF FM */ new URL("http://31.192.216.4:8000/rmf_fm"));
+                put(RemoteControlCommand.CH5, /* RMF MAXXX */ new URL("http://217.74.72.10:8000/rmf_maxxx"));
+                put(RemoteControlCommand.CH6, /* RMF CLASSIC */ new URL("http://195.150.20.242:8000/rmf_classic"));
+                put(RemoteControlCommand.CH7, /* ESKA TROJMIASTO */ new URL("http://waw04.ic1.scdn.smcloud.net/t048-1.mp3"));
+                put(RemoteControlCommand.CH8, /* RADIO GDANSK */ new URL("http://stream.task.gda.pl:8000/rg1"));
+                put(RemoteControlCommand.CH9, /* TOK FM */ new URL("http://wroclaw.radio.pionier.net.pl:8000/pl/tuba10-1.mp3"));
             }};
         } catch (MalformedURLException e) {
             logger.error("Error when setting up radio stations list on app start", e);
